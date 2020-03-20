ea9b49a85fc6801b03dd720444fe61c057721aed
keithhead <37887616+keithhead@users.noreply.github.com>
2020-03-20 16:14:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 
 app.use('/static', express.static(__dirname + '/public'));
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.post('/update', function(req, res) {
