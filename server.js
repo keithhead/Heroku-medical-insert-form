add9a11cf3cb7eccc55f23e97d2435f210c3eaf3
keithhead <37887616+keithhead@users.noreply.github.com>
2020-03-23 23:18:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var app = express();
+var nforce = require('nforce');
+var oauth;
 
 app.set('port', process.env.PORT || 5000);
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
-var nforce = require('nforce');
 
 var org = nforce.createConnection({
   clientId: '3MVG9G9pzCUSkzZtzL9w0ddPTxvREi8INdGWwqqEXmesmLTvLPters6NbZ4vMeFHJ__EDI.YP2GGJlygl1_NJ',
@@ -16,11 +15,8 @@ var org = nforce.createConnection({
   redirectUri: 'https://login.salesforce.com/oauth2/callback',
 });
 
-var oauth;
-
 org.authenticate({ username: 'king.lai@playful-goat-5h00v6.com', password: 'S6tw1515', securityToken: 'WmSDOZZzVO2GxapgaOnoqm17' }, function(err, resp){
   // the oauth object was stored in the connection object
-  
   if(!err) {
     console.log('RESP: ' + JSON.stringify(resp));
     console.log('Access Token: ' + resp.access_token);
@@ -33,7 +29,7 @@ org.authenticate({ username: 'king.lai@playful-goat-5h00v6.com', password: 'S6tw
 app.post('/addData', function(req, res) {
     var acc = nforce.createSObject('Account');
     acc.set('Name', req.body.name);
-    acc.set('Phone', '800-555-2345');
+    acc.set('Phone', req.body.phone);
 
     org.insert({ sobject: acc, oauth: oauth }, function(err, resp){
       if(!err) console.log('It worked!');
