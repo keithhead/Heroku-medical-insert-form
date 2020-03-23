a0b3cc28177dbba1a6b2f8529348c46c47a9362d
keithhead <37887616+keithhead@users.noreply.github.com>
2020-03-23 22:52:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,19 @@ var org = nforce.createConnection({
   redirectUri: 'https://login.salesforce.com/oauth2/callback',
 });
 
+var oauth;
+
 org.authenticate({ username: 'king.lai@playful-goat-5h00v6.com', password: 'S6tw1515', securityToken: 'WmSDOZZzVO2GxapgaOnoqm17' }, function(err, resp){
   // the oauth object was stored in the connection object
-  if(!err) console.log('Cached Token: ' + org.oauth.access_token)
-  if (err) console.log(err);
+  
+  if(!err) {
+    console.log('RESP: ' + resp);
+    console.log('Access Token: ' + resp.access_token);
+    oauth = resp;
+  } else {
+    console.log('Error: ' + err.message);
+  }
+
 //   var acc = nforce.createSObject('Account');
 //   acc.set('Name', 'Spiffy Cleaners');
 //   acc.set('Phone', '800-555-2345');
