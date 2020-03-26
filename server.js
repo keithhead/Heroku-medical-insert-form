074837a58dc49fe37d1cacbf41d34e51573cd485
keithhead <37887616+keithhead@users.noreply.github.com>
2020-03-26 15:26:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var app = express();
-var nforce = require('nforce');
+var nforce = require('nforce'), chatter = require('nforce-chatter')(nforce),
 var oauth;
 
 app.set('port', process.env.PORT || 5000);
@@ -13,6 +13,7 @@ var org = nforce.createConnection({
   clientId: '3MVG9G9pzCUSkzZs3g2TYsOpgn9hsinhjm2vDfQpI_VVRK3E3V4E.9synPARSsrriKC58K47OyctfhZ60kJAt',
   clientSecret: 'E11C84E8ADA88C419068838F3E17ACF1E27C31273D3DDAFA168B7A3CB8D9B930',
   redirectUri: 'https://login.salesforce.com/oauth2/callback',
+  plugins: ['chatter']
 });
 
 org.authenticate({ username: 'keith.ng@k-su19.demo', password: 'salesforce1', securityToken: '27oCbWWf4v5EAwliCXTBsj8Q' }, function(err, resp){
